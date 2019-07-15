80462d64197da669fc76ab11f75b427f065aa4f4
DryFear <voronin8945@mail.ru>
2019-07-15 22:56:05 +0300

    Added nickname input For what? Because i can)

--- app/src/main/java/ru/unfortunately/aboutme/MainActivity.kt
+++ app/src/main/java/ru/unfortunately/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package ru.unfortunately.aboutme
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText: EditText = findViewById(R.id.nickname_edit)
+        val nicknameTextView: TextView = findViewById(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
